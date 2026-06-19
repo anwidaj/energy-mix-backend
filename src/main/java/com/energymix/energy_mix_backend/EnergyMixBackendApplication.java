8a3f536a19a4f368cc19a63abc7e4f6777aa8b65
jar <reavo28@gmail.com>
2026-06-19 16:43:10 +0200

    Initial commit

---
+++ src/main/java/com/energymix/energy_mix_backend/EnergyMixBackendApplication.java
@@ -0,0 +1,13 @@
+package com.energymix.energy_mix_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergyMixBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergyMixBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/energymix/energy_mix_backend/EnergyMixBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.energymix.energy_mix_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergyMixBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
